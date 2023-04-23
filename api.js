1f6884da43ab4affc270f90fb15bed58d99f5df0
AhtritCodes <tirtha13july2002sonofmp@gmail.com>
2023-04-23 06:31:05 +0530

    updated

--- api.js
+++ api.js
@@ -38,7 +38,7 @@ fs.createReadStream('Microbes_pH.csv')
 // Read CSV file of temp and store its contents in an array
 const results_temp = [];
 
-fs.createReadStream('MicrobialGrowth-temp-Data.csv')
+fs.createReadStream('Microbes_temperature.csv')
 .pipe(csvParser())
 .on('data', (rows) => {
 	results_temp.push(rows);
